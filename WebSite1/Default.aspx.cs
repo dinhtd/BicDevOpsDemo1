48e8dce9f7319ed3ce22900419eb1a332dd8defe
dinhtd <86462518+dinhtd@users.noreply.github.com>
2021-06-28 10:57:09 +0700

    Update Default.aspx.cs

--- WebSite1/Default.aspx.cs
+++ WebSite1/Default.aspx.cs
@@ -10,5 +10,6 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         var message = "Welcome Bic DevOps Demo!";
+        var a = 1;
     }
 }
